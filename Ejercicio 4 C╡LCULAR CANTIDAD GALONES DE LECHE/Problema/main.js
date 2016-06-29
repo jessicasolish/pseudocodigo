199e035057d526e0a6b2251ecd46f75bd6969ce6
jessicasolish <jessica.solis.h@gmail.com>
2016-06-29 12:09:23 -0300

    pseudocodigo

--- Ejercicio 4 C╡LCULAR CANTIDAD GALONES DE LECHE/Problema/main.js
+++ Ejercicio 4 C╡LCULAR CANTIDAD GALONES DE LECHE/Problema/main.js
@@ -1,1 +1,11 @@
+var d = 0;
+var promedioUno = 0;
+function btn41() {
+	while(d < 5) {
+		numero = Number(prompt("Porfavor ingresa tu número " + (d+1)));
+		promedioUno = promedioUno + numero; // Se almacena la suma
+		d++;
+	}
+	document.getElementById("ej41").innerHTML = "El promedio de tus números es " + promedioUno/5;
+}
 //Escribe aquí tu código
